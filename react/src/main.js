bf1f17c81762bc19eb8cad2310859602c1ff3b31
Corey Fischer <fischer0525@gmail.com>
2017-04-10 15:00:51 -0400

    Initial commit

---
+++ react/src/main.js
@@ -0,0 +1,14 @@
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+$(function() {
+  let reactApp = document.getElementById()
+  if(reactApp){
+    ReactDOM.render(
+      < />,
+      reactApp
+    );
+  };
+});
---
+++ react/webpack.config.js
@@ -0,0 +1,34 @@
+var config = {
+  entry: {
+    path: './react/src/main.js',
+  },
+  output: {
+    path: './app/assets/javascripts',
+    filename: 'bundle.js'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'url?limit=25000'
+      }
+    ]
+  },
+  devtool: 'eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  delete config.devtool;
+  var webpack = require('webpack');
+  config.plugins = [
+    new webpack.DefinePlugin({ 'process.env.NODE_ENV': '"production"' })
+  ];
+}
+
+module.exports = config;
